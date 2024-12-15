be1d2d069e58d1f5380f6f43fbf48000ea4757e0
Rishav Chudal <rishav.chudal@esewa.com.np>
2024-12-16 03:35:26 +0545

    Initial Commit

---
+++ Card Generator/Card_GeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Card_GeneratorApp.swift
+//  Card Generator
+//
+//  Created by Rishav-Chudal on 16/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct Card_GeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
